44a16462a4eeada11e14d379be7536d40391a2e8
Miguel Figuera <miguelqui725@gmail.com>
2023-03-02 00:24:16 -0400

    still in mailers

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,7 @@
+class UserMailer < ApplicationMailer
+    default from: "ror.book@gmail.com"
+
+    def welcome_mail
+        @user=params[:user]
+        mail(to: @user.email, subject: "Welcome to ror-book!")
+end
