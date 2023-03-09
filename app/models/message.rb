c6966623a6faa3f93616b8386536ab19503e1bbe
Miguel Figuera <miguelqui725@gmail.com>
2023-03-09 17:57:20 -0400

    finishing chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,10 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
+  #scopes
   scope :room_messages,->(room){where("room_id = ?",room.id)}
+
+
+  #broadcasts
+  after_create_commit -> { broadcast_append_to 'lol', partial: "messages/message", locals: {quote: self},target: 'lol'}
 end
